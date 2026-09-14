45694fe69fac1ac83ce74d8ae2496e18790ef8bb
WongTingWai26-27 <bw353504@gmail.com>
2026-09-14 14:46:19 +0000

    [skip actions] lab/lab01/ch01_t01_hello_world.py

--- lab/lab01/ch01_t01_hello_world.py
+++ lab/lab01/ch01_t01_hello_world.py
@@ -1,4 +1,4 @@
 print("Hello, world!")
-print("Water-there is not a drop of water there!Were Niagara but a cataract of sand" \
+print("Water-there is not a drop of water there!Were Niagara but a cataract of sand,
 "")
       
